a7d4c0f8d4a57a53f94c765ba73fc671928ae403
Amin Mojarad <aminmojarad1@gmail.com>
2025-09-07 15:44:09 -0400

    Add loop to assign event listeners to number btns
    Add comments to explain resultState variable

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 // Select all button elements
-const oneBtn = document.getElementById('one');
-const twoBtn = document.getElementById('two');
-const threeBtn = document.getElementById('three');
-const fourBtn = document.getElementById('four');
-const fiveBtn = document.getElementById('five');
-const sixBtn = document.getElementById('six');
-const sevenBtn = document.getElementById('seven');
-const eightBtn = document.getElementById('eight');
-const nineBtn = document.getElementById('nine');
-const zeroBtn = document.getElementById('zero');
 const decimalBtn = document.getElementById('decimal');
 const addBtn = document.getElementById('add');
 const subtractBtn = document.getElementById('subtract');
@@ -21,6 +11,7 @@ const display = document.getElementById('display');
 const currDisplay = document.getElementById('result');
 const history = document.getElementById('history');
 const negativeBtn = document.getElementById('negative');
+const numberBtns = document.querySelectorAll('.number');
 
 // Initialize state variables
 
@@ -29,23 +20,17 @@ let clearOnNext = false;
 // operators array for easy checking
 const operators = ['+', '-', '*', '/'];
 // make resultr state variable to track if we just calculated a result
-// this allows us to ignore negative sign issues in the history that may be confused for an operation
+// this allows us to ignore negative sign issues in the history that may be confused for an operation 
+// (e.g. 5 + -3 was being picked up as a minus operation, this allows us to avoid that)
 let resultState = false;
 // make operation state variable to track if an operation button was pressed
 //this allows us to handle consecutive operations clicks correctly
 let operationState = false;
 
 // Add event listeners to each button
-oneBtn.addEventListener('click', () => pressNumber("1"));
-twoBtn.addEventListener('click', () => pressNumber("2"));
-threeBtn.addEventListener('click', () => pressNumber("3"));
-fourBtn.addEventListener('click', () => pressNumber("4"));
-fiveBtn.addEventListener('click', () => pressNumber("5"));
-sixBtn.addEventListener('click', () => pressNumber("6"));
-sevenBtn.addEventListener('click', () => pressNumber("7"));
-eightBtn.addEventListener('click', () => pressNumber("8"));
-nineBtn.addEventListener('click', () => pressNumber("9"));
-zeroBtn.addEventListener('click', () => pressNumber("0"));
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => pressNumber(button.getAttribute('data-value')));
+});
 decimalBtn.addEventListener('click', pressDecimal);
 clearBtn.addEventListener('click', () => {currDisplay.textContent = '0'});
 subtractBtn.addEventListener('click', subtract);
